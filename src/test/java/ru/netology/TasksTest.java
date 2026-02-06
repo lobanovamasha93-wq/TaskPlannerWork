06f42ac357f2f12021bed0c225519387407f88c4
Maria Lobanova <marusa.lobanova2015@yandex.ru>
2026-02-06 18:32:02 +0400

    Implement Task hierarchy and search logic + test coverage for search logic

---
+++ src/test/java/ru/netology/TasksTest.java
@@ -0,0 +1,58 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+
+    @Test
+    public void shouldMatchSimpleTaskExactQuery() {
+        SimpleTask simpleTask = new SimpleTask(1, "Купить хлеб");
+        boolean matches = simpleTask.matches("хлеб");
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    public void shouldNotMatchSimpleTaskIncorrectQuery() {
+        SimpleTask simpleTask = new SimpleTask(1, "Купить хлеб");
+        boolean matches = simpleTask.matches("молоко");
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
+    public void shouldMatchEpicSubtask() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(2, subtasks);
+        boolean matches = epic.matches("Яйца");
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    public void shouldNotMatchEpicIncorrectQuery() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(2, subtasks);
+        boolean matches = epic.matches("Масло");
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
+    public void shouldMatchMeetingTopic() {
+        Meeting meeting = new Meeting(3, "Выкатка 3й версии", "Приложение НетоБанка", "Во вторник");
+        boolean matches = meeting.matches("версии");
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    public void shouldMatchMeetingProject() {
+        Meeting meeting = new Meeting(3, "Выкатка 3й версии", "Приложение НетоБанка", "Во вторник");
+        boolean matches = meeting.matches("НетоБанка");
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    public void shouldNotMatchMeetingIncorrectQuery() {
+        Meeting meeting = new Meeting(3, "Выкатка 3й версии", "Приложение НетоБанка", "Во вторник");
+        boolean matches = meeting.matches("Среда");
+        Assertions.assertFalse(matches);
+    }
+}
---
+++ src/test/java/ru/netology/TodosTest.java
@@ -0,0 +1,71 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchTasksByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Купить Хлеб");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks); // Подходит, так как есть "Хлеб"
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        // Ищем "Хлеб". Должны найтись SimpleTask и Epic
+        Task[] expected = {simpleTask, epic};
+        Task[] actual = todos.search("Хлеб");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchTasksByQueryReturnsEmptyArrayWhenNoneFound() {
+        SimpleTask simpleTask = new SimpleTask(5, "Купить Хлеб");
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Космос");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
